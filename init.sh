5a882f052a1ce5c095597f54dff7e338c47f3e46
MrLokans <trikster1911@gmail.com>
2016-03-09 14:23:40 +0300

    Nginx proxy configuration added.

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 DEFAULT_CONFIG=/etc/nginx/sites-enabled/default                                 
-CONF_LINK=/etc/nginx/sites-enabled/test.conf                                    
+CONF_LINK=/etc/nginx/sites-enabled/test.conf
+GUNICORN_CONF=//etc/gunicorn.d/hello.py                                    
                                                                                 
 if [ -f $DEFAULT_CONFIG ]; then                                                 
     sudo rm $DEFAULT_CONFIG                                                     
@@ -7,8 +8,13 @@ fi
                                                                                 
 if [ -f $CONF_LINK ]; then                                                      
     sudo rm $CONF_LINK                                                          
+fi;
+
+if [ -f $GUNICORN_CONF ]; then
+    sudo rm $GUNICORN_CONF
 fi;                                                                             
                                                                                 
                                                                                 
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf      
+sudo ln -s /home/box/web/etc/nginx.conf $CONF_LINK     
+sudo ln -s /home/box/web/etc/hello.py $GUNICORN_CONF     
 sudo /etc/init.d/nginx restart
