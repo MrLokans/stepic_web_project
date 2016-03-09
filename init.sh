34494cdd0632d6987d2a029780c821db018215d9
MrLokans <trikster1911@gmail.com>
2016-03-09 14:05:32 +0300

    Nginx config and bootstrap scripts are updated.

--- init.sh
+++ init.sh
@@ -1,8 +1,14 @@
-DEFAULT_CONFIG=/etc/nginx/sites-enabled/default
-
-if [ ! -f DEFAULT_CONFIG ]; then
-    sudo rm /etc/nginx/sites-enabled/default
-fi
-
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+DEFAULT_CONFIG=/etc/nginx/sites-enabled/default                                 
+CONF_LINK=/etc/nginx/sites-enabled/test.conf                                    
+                                                                                
+if [ -f $DEFAULT_CONFIG ]; then                                                 
+    sudo rm $DEFAULT_CONFIG                                                     
+fi                                                                              
+                                                                                
+if [ -f $CONF_LINK ]; then                                                      
+    sudo rm $CONF_LINK                                                          
+fi;                                                                             
+                                                                                
+                                                                                
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf      
 sudo /etc/init.d/nginx restart
