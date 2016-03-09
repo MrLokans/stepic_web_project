bf4db72a1e72ce2c375319685f1ecd285cf8a0d0
MrLokans <trikster1911@gmail.com>
2016-03-09 13:46:59 +0300

    NGINX config file added. Bootstrap script updated.

--- init.sh
+++ init.sh
@@ -1,2 +1,8 @@
+DEFAULT_CONFIG=/etc/nginx/sites-enabled/default
+
+if [ ! -f DEFAULT_CONFIG ]; then
+    sudo rm /etc/nginx/sites-enabled/default
+fi
+
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
